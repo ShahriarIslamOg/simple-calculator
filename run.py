7eacb1c38aaa75fc8381344b450715616df2cf73
Shahriar Islam <85397003+ByteShahriar@users.noreply.github.com>
2023-12-04 10:39:38 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,56 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    try:
+        result = x / y
+        return result
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
+
+while True:
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Exiting the calculator.")
+        break
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter numeric values.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            result = divide(num1, num2)
+            print(num1, "/", num2, "=", result)
+
+            if isinstance(result, str):
+                print(result)  # Print error message for division by zero
+
+    else:
+        print("Invalid Input. Please enter a valid option (1/2/3/4/5).")
